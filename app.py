2523d3136a8c4a03bff851a738b1c7bf475dc9d0
ssklyg36 <97388321+ssklyg36@users.noreply.github.com>
2022-07-28 16:35:05 +0630

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/LadiaYang/Documents/Programming/MSI_summer/Notesapp/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
